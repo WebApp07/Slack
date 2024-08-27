72ae553677cd09f58001f4e270fb8d08f32531c7
unknown <aminezouguig@proton.me>
2024-08-27 16:18:20 +0200

    Ui Sign-in Sign-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
     <>
-      <div className="text-rose-500 font-bold text-2xl">
+      <div className="text-white font-bold text-2xl">
         <AuthScreen />
       </div>
     </>
